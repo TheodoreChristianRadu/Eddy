a94b967956d97e7f5553836d9813123c13c65d8d
Theodore Radu <theodorechristianradu@gmail.com>
2022-03-18 02:30:15 +0100

    Add project files.

---
+++ Eddy/Eddy.py
@@ -0,0 +1,26 @@
+﻿import re
+from collections import defaultdict
+from random import choices
+
+class Eddy():
+
+    def __init__(self):
+        self.chain = defaultdict(lambda: defaultdict(lambda: 0))
+
+    def influence(self, text):
+        text = re.sub(r"[^a-zA-ZÀ-ÿ\s'-]", "", text).lower()
+        words = text.split()
+        for i in range(len(words)):
+            self.chain[words[i - 1]][words[i]] += 1 / len(words)
+
+    def speak(self, length):
+        options = list(self.chain.keys())
+        if (length == 0 or len(options) == 0):
+            return "Je vous en prie."
+        words = choices(options)
+        for i in range(length - 1):
+            options = list(self.chain[words[i]].keys())
+            weights = list(self.chain[words[i]].values())
+            words += choices(options, weights)
+        words[0] = words[0].capitalize()
+        return " ".join(words) + "."
---
+++ Eddy/Main.py
@@ -0,0 +1,30 @@
+﻿from pyforms.basewidget import BaseWidget
+from pyforms.controls import ControlFile, ControlSlider, ControlButton, ControlTextArea
+from pyforms import start_app
+from Eddy import Eddy
+
+class Program(BaseWidget):
+
+    def __init__(self):
+        self.eddy = Eddy()
+        super().__init__("Eddy")
+        self.file = ControlFile("Influence")
+        self.file.changed_event = self.loadFile
+        self.length = ControlSlider("Length")
+        self.length.max = 1000
+        self.button = ControlButton("Speak")
+        self.button.value = self.showSpeech
+        self.text = ControlTextArea()
+        self.formset = ["", ("", "file", "length", "button", ""), "", ("", "text", ""), ""]
+
+    def loadFile(self):
+        try:
+            with open(self.file.value, encoding="utf-8") as data:
+                self.eddy.influence(data.read())
+        except FileNotFoundError:
+            pass
+
+    def showSpeech(self):
+        self.text += "Eddy : " + self.eddy.speak(self.length.value) + "\n"
+
+start_app(Program)
