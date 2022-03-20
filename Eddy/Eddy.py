c60c7a91e0d504f3c53cfab086894bb457bf8d79
Theodore Radu <theodorechristianradu@gmail.com>
2022-03-20 21:06:26 +0100

    IronPython UI

--- Eddy/Eddy.py
+++ Eddy/Eddy.py
@@ -1,19 +1,33 @@
-﻿from Text import split, combine
-from Chain import Chain, ChainGroup
+﻿import wpf
+from System.Windows import Application, Window
+from Microsoft.Win32 import OpenFileDialog
+from Text import split, combine
+from Chains import Chain, ChainGroup
 
-class Eddy():
+class Eddy(Window):
 
     def __init__(self):
-        self.chains = ChainGroup()
+        wpf.LoadComponent(self, 'Window.xaml')
+        self.speakButton.Click += self.speak
+        self.loadButton.Click += self.load
+        self.chain = ChainGroup()
 
-    def influence(self, text):
-        words = split(text)
-        self.chains.append(Chain(words))
+    def load(self, *args):
+        dialog = OpenFileDialog()
+        dialog.Filter = "Text|*.txt"
+        if dialog.ShowDialog():
+            with open(dialog.FileName, encoding="utf-8") as data:
+                words = split(data.read())
+                self.chain.append(Chain(words))
 
-    def speak(self, length):
+    def speak(self, *args):
         words, word = [], "."
-        while len(words) <= length or word != ".":
+        while len(words) <= 0 or word != ".":
             word = self.chain(word)
             if word == "": break
             words.append(word)
-        return combine(words)
+        self.textbox.Text += "Eddy : " + combine(words) + "\n"
+        self.textbox.ScrollToEnd()
+
+if __name__ == '__main__':
+    Application().Run(Eddy())
